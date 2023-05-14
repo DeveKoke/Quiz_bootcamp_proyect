9b6e463f0a583060f37d0812bb1b7816be548373
DeveKoke <jorge.be86@gmail.com>
2023-05-14 17:11:05 +0200

    html_first_question_structure

---
+++ index.js
@@ -0,0 +1,10 @@
+
+
+// * crea un EventListener para todos los botones incorrectos.
+const btns = document.querySelectorAll('.btn');
+
+btns.forEach(btn => {
+  btn.addEventListener('click', function() {
+    console.log('¡Haz hecho clic en un botón!');
+  });
+});
